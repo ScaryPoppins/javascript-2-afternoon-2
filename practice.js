9dfb4dfaaf49477a2a995c7f28a34c20ee0b11f3
Mark Nagel <mark_nagel@hotmail.com>
2019-05-02 10:12:07 -0500

    Mark Nagel second commit

--- practice.js
+++ practice.js
@@ -85,13 +85,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+
+
+var evenNums = [];
+
 function evenFinder(nums){
   for (var i = 0; i < nums.length; i++){
     if(nums[i] % 2 === 0){
       evenNums.push(nums[i]);
     }
   }
-
+  return evenNums;
 }
 
 
